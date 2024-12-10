ae043c041628bba7d87aff739df00a93b2f4a4a7
MatheusBProgrammer <matheusbarretoprogramador@example.com>
2024-12-10 18:38:12 -0300

    first commit

---
+++ src/main/java/com/chamadaunivs/chamadaunivs/ChamadaunivsApplication.java
@@ -0,0 +1,13 @@
+package com.chamadaunivs.chamadaunivs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamadaunivsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamadaunivsApplication.class, args);
+	}
+
+}
